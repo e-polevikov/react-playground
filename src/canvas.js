2c3b7a880739e4a9d8ab99bce6d0e1d208528bb8
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-11-05 21:04:31 +0300

    Add functionality for moving circles

--- src/canvas.js
+++ src/canvas.js
@@ -17,24 +17,39 @@ const INITIAL_STATE = generateCircles();
 function Canvas() {
   const [circles, setCircles] = React.useState(INITIAL_STATE);
 
+  const moveCirclesHandler = (e) => {
+    setCircles(
+      circles.map((circle) => {
+        circle.x = circle.x + (Math.random() - 0.5) * 10;
+        circle.y = circle.y + (Math.random() - 0.5) * 10;
+        return circle;
+      })
+    );
+  };
+
   return (
-    <div className='stage'>
-      <Stage width={canvasWidth} height={canvasHeight}>
-        <Layer>
-          {circles.map((circle) => (
-            <Circle
-              key={circle.id}
-              id={circle.id}
-              x={circle.x}
-              y={circle.y}
-              radius={20}
-              fill='red'
-              draggable
-            />
-          ))}
-        </Layer>
-      </Stage>
-    </div>
+    <>
+      <div className='stage'>
+        <Stage width={canvasWidth} height={canvasHeight}>
+          <Layer>
+            {circles.map((circle) => (
+              <Circle
+                key={circle.id}
+                id={circle.id}
+                x={circle.x}
+                y={circle.y}
+                radius={20}
+                fill='red'
+                draggable
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+      <div>
+        <button onClick={moveCirclesHandler}>Move Circles Randomly</button>
+      </div>
+    </>
   );
 }
 
