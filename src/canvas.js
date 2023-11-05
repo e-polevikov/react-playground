61a508248c0a6f1de3cc1a47d28ff1888eb4ba83
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-11-05 20:16:33 +0300

    Replace starts with circles

--- src/canvas.js
+++ src/canvas.js
@@ -1,73 +1,35 @@
 import React from 'react';
-import {Stage, Layer, Star} from 'react-konva';
+import {Stage, Layer, Circle} from 'react-konva';
 
 const canvasWidth = 700;
 const canvasHeight = 350;
 
-function generateShapes() {
+function generateCircles() {
   return [...Array(10)].map((_, i) => ({
     id: i.toString(),
     x: Math.random() * canvasWidth,
-    y: Math.random() * canvasHeight,
-    rotation: Math.random() * 180,
-    isDragging: false,
+    y: Math.random() * canvasHeight
   }));
 }
 
-const INITIAL_STATE = generateShapes();
+const INITIAL_STATE = generateCircles();
 
 function Canvas() {
-  const [stars, setStars] = React.useState(INITIAL_STATE);
-
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: star.id === id,
-        };
-      })
-    );
-  };
-
-  const handleDragEnd = (e) => {
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: false,
-        };
-      })
-    );
-  };
+  const [circles, setCircles] = React.useState(INITIAL_STATE);
 
   return (
     <div className='stage'>
       <Stage width={canvasWidth} height={canvasHeight}>
         <Layer>
-          {stars.map((star) => (
-            <Star
-              key={star.id}
-              id={star.id}
-              x={star.x}
-              y={star.y}
-              numPoints={5}
-              innerRadius={20}
-              outerRadius={40}
-              fill="#89b717"
-              opacity={0.8}
-              //draggable
-              rotation={star.rotation}
-              shadowColor="black"
-              shadowBlur={10}
-              shadowOpacity={0.6}
-              shadowOffsetX={star.isDragging ? 10 : 5}
-              shadowOffsetY={star.isDragging ? 10 : 5}
-              scaleX={star.isDragging ? 1.2 : 1}
-              scaleY={star.isDragging ? 1.2 : 1}
-              //onDragStart={handleDragStart}
-              //onDragEnd={handleDragEnd}
+          {circles.map((circle) => (
+            <Circle
+              key={circle.id}
+              id={circle.id}
+              x={circle.x}
+              y={circle.y}
+              radius={20}
+              fill='red'
+              draggable
             />
           ))}
         </Layer>
