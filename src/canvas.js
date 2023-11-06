a29f626240bb0c3a2891ba28d5bb101614da4b2d
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-11-06 22:13:59 +0300

    Add functionality to move circles without intersection

--- src/canvas.js
+++ src/canvas.js
@@ -3,23 +3,27 @@ import {Stage, Layer, Circle} from 'react-konva';
 
 const canvasWidth = 700;
 const canvasHeight = 350;
-const circleRadius = 20;
-const initialNumberOfCircles = 10;
+const circleRadius = 30;
+const initialNumberOfCircles = 15;
 
-function checkIntersection(circles, randX, randY) {
-  let randomCircleIntersecsExisting = false;
+function checkIntersection(circles, randX, randY, circleId) {
+  let circleIntersecsExisting = false;
 
   for (let i = 0; i < circles.length; i++) {
+    if (i === circleId) {
+      continue;
+    }
+
     let xDistance = (circles[i].x - randX) * (circles[i].x - randX);
     let yDistance = (circles[i].y - randY) * (circles[i].y - randY);
 
     if (xDistance + yDistance <= 4 * circleRadius * circleRadius) {
-      randomCircleIntersecsExisting = true;
+      circleIntersecsExisting = true;
       break;
     }
   }
 
-  return randomCircleIntersecsExisting;
+  return circleIntersecsExisting;
 }
 
 function generateCircles() {
@@ -29,9 +33,13 @@ function generateCircles() {
   while (numberOfGeneratedCircles < initialNumberOfCircles) {
     let randX = Math.random() * canvasWidth;
     let randY = Math.random() * canvasHeight;
+    let generatedCircleId = numberOfGeneratedCircles + 1;
 
-    if (!checkIntersection(circles, randX, randY)) {
-      circles.push({id: numberOfGeneratedCircles.toString(), x: randX, y: randY});
+    if (!checkIntersection(circles, randX, randY, generatedCircleId)) {
+      circles.push({
+        id: numberOfGeneratedCircles.toString(),
+        x: randX, y: randY
+      });
       numberOfGeneratedCircles += 1;
     }
   }
@@ -47,8 +55,20 @@ function Canvas() {
   const moveCirclesHandler = (e) => {
     setCircles(
       circles.map((circle) => {
-        circle.x = circle.x + (Math.random() - 0.5) * 10;
-        circle.y = circle.y + (Math.random() - 0.5) * 10;
+        let circleIsMoved = false;
+
+        while (!circleIsMoved) {
+          let randX = circle.x + (Math.random() - 0.5) * 10;
+          let randY = circle.y + (Math.random() - 0.5) * 10;
+          let currentCircleId = Number(circle.id);
+
+          if (!checkIntersection(circles, randX, randY, currentCircleId)) {
+            circle.x = randX;
+            circle.y = randY;
+            circleIsMoved = true;
+          }
+        }
+
         return circle;
       })
     );
