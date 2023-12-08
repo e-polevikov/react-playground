fd6f96467e1e38b9e2317d9594b8ef53648ff690
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-12-08 18:35:51 +0300

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+function App() {
+  return (
+    <>
+      <h1 className='hello-message'>Hello!</h1>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
