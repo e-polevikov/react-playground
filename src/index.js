eddab167e8b6e261202e68e4f60e2ae6034b5476
Eugene Polevikov <eugene.polevikov@yandex.ru>
2023-12-19 16:14:52 +0300

    Cleanup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function App() {
-  return (
-    <div>
-      <h1 className='hello-message'>Hello!</h1>
-    </div>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
